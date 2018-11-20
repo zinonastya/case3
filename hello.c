53aaa3261be05ed750be4df6f677a85c0c68b29f
zinoveva <zinonastya98@gmail.com>
2018-11-20 21:05:43 +0300

    first

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "hello.h"
+
+void
+print_hello(void) {
+    printf("World!\n");
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hello.h"
+
+int main(void) {
+    printf("Hello, ");
+    print_hello();
+    exit(0);
+}
